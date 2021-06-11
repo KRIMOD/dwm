c32bec7a97569b404e1c04a3263249a233875013
Abdelkrim Temam <gm_temam@esi.com>
2021-06-12 00:13:21 +0100

    added slock shortcut

--- config.h
+++ config.h
@@ -148,6 +148,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_q,			killclient,	{0} },
 	// { MODKEY|ShiftMask,		XK_q,		spawn,		SHCMD("sysact") },
 	{ MODKEY|ShiftMask,		XK_q,		spawn,					SHCMD("killall dwm") },
+	{ MODKEY|ShiftMask,		XK_l,		spawn,					SHCMD("slock") },
 	{ MODKEY,							XK_w,		spawn,					SHCMD("$BROWSER") },
 	{ MODKEY|ShiftMask,		XK_w,		spawn,					SHCMD(TERMINAL " -e sudo nmtui") },
 	{ MODKEY,							XK_e,		spawn,					SHCMD(TERMINAL " -e neomutt ; pkill -RTMIN+12 dwmblocks; rmdir ~/.abook") },
